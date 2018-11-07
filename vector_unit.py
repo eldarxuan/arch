58e17cecf72ddaf05fcc5afe27a50b62e4406bf6
Yuxuan Xu <852563495@qq.com>
2018-11-07 21:00:53 +0800

    chage int(1 >> 1.0)

--- vector_unit.py
+++ vector_unit.py
@@ -1,4 +1,4 @@
     def unit(self):
         # 单位化一个向量
-        unit_vec = Vector(self.x/self.length, self.y/self.length, length = 1)
+        unit_vec = Vector(self.x/self.length, self.y/self.length, length = 1.0)
         return unit_vec
