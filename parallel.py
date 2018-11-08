ab165de27a252e6c12ed116060b5cce966d6756d
Yuxuan Xu <852563495@qq.com>
2018-11-08 14:52:34 +0800

    replace "for"

--- parallel.py
+++ parallel.py
@@ -12,6 +12,6 @@
     def isParallel(self):
         # 根据向量计算两个矩形是否平行
         parallel = False
-        if self.rec1.vec_lst[0].isVectorParallel(self.rec2.vec_lst[0]) == True:
-		    parallel = True
+        if self.rec1.vec_lst[0].isVectorParallel(self.rec2.vec_lst[0])  == True or self.dot(self.rec1.vec_lst[0],self.rec2.vec_lst[0]) == 0:
+		    parallel = True   #两矩形内任意两向量垂直或平行，两矩形平行
         return parallel
