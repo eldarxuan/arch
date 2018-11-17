2371557b023b6c0e742437a44d9535f13da2c62c
Yuxuan Xu <852563495@qq.com>
2018-11-17 14:14:42 +0800

    城市设计_类

--- parallel.py
+++ parallel.py
@@ -13,5 +13,5 @@
         # 根据向量计算两个矩形是否平行
         parallel = False
         if self.rec1.vec_lst[0].isVectorParallel(self.rec2.vec_lst[0]) == True or self.dot(self.rec1.vec_lst[0],self.rec2.vec_lst[0]) == 0:
-		    parallel = True   #两矩形内任意两向量垂直或平行，两矩形平行
+            parallel = True   #两矩形内任意两向量垂直或平行，两矩形平行
         return parallel
